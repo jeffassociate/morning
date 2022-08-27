d3358af7e53a25c5a5e083f51d89f5a0cdc3a1bd
jeffassociate <59381739+jeffassociate@users.noreply.github.com>
2022-08-27 22:09:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ def get_weatherhigh():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['high'])
+  return math.floor(weather['high'])
   
 def get_weatherlow():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['low'])
+  return math.floor(weather['low'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
